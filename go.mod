6baa3009d2997e4c52db45e4c7fbd3020cc289ce
chi2l3s <amixolplex@gmail.com>
2026-02-14 15:29:25 +0700

    feat: remove old workflows and add new publish workflows for NPM and Go, update protobuf generation scripts

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lumina-cinema/contracts
+
+go 1.25.4
---
+++ scripts/generate_go.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "Generating Go protobuf (media)..."
+
+protoc -I ./proto \
+    --go_out=./gen/go \
+    --go-grpc_out=./gen/go \
+    --go_opt=module=github.com/lumina-cinema/contracts \
+    --go-grpc_out=module=github.com/lumina-cinema/contracts \
+    ./proto/media.proto
+
+echo "Go generation complete"
